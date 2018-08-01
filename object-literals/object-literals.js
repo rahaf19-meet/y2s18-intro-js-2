0faa8ef9cfeabc0b681d390c5d41b9b0d5809e51
rahaf19-meet <rahaf19@meet.mit.edu>
2018-08-01 07:12:05 -0400

    jhalaas

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,9 @@
 // Create your 'me' object literal here!
+var ME = {
+	firstName: "Rahaf",
+	lastName:"Sabri",
+	favoritefood:["french fries","pasta","wara 3nb"],
+	age:15,
+	picture:"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj-n7bv0svcAhVJyhoKHavuApIQjRx6BAgBEAU&url=http%3A%2F%2Fcooldigital.photography%2Fmost-inspirational-wallpapers-backgrounds%2F&psig=AOvVaw20r1r2W2vZZnz6f5LXgpZS&ust=1533205804964998",
+}
+document.write(JSON.stringify(ME))
