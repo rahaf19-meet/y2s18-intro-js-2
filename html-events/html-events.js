c95e40239206e38facad1fd4e66e37f1e8822628
rahaf19-meet <rahaf19@meet.mit.edu>
2018-08-01 18:34:43 +0300

    fg

--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,7 @@
 // Put all your JavaScript in this file!
+function COLOR() {
+	var back=prompt("write down a color!")
+	// body...
+
+v=document.getElementsByClassName('g')
+v[0].style.backgroundColor=back}
